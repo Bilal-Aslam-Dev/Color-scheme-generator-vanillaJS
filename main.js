0ea10e8944c1324c5049244c87dbf9e8cadd4495
bilal1999-cyber <103093008+bilal1999-cyber@users.noreply.github.com>
2022-08-13 18:51:57 +0500

    upgrade/// copy to clipboard

--- main.js
+++ main.js
@@ -3,17 +3,59 @@ var colors = document.querySelectorAll(".color-shade")
 var schemeType = document.getElementById("scheme-type")
 var colorSchemeCode = document.querySelectorAll('.color-scheme-code')
 
-document.querySelector(".color-scheme-btn").addEventListener("click", () => {
-    fetch(`https://www.thecolorapi.com/scheme?hex=${colorInput.value.substring(1)}&mode=${schemeType.options[schemeType.selectedIndex].value}`)
-    .then((response) => response.json())
-    .then((data) => {
-        data.colors.map((elem, index) => {
-            colors[index].style.backgroundColor = data.colors[index].hex.value
-            colorSchemeCode[index].textContent = data.colors[index].hex.value 
+function colorFetcher() {
+    var schemeVal = schemeType.options[schemeType.selectedIndex].value
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorInput.value.substring(1)}&mode=${schemeVal}`)
+        .then((response) => response.json())
+        .then((data) => {
+            
+            data.colors.map((elem, index) => {
+                colors[index].style.backgroundColor = data.colors[index].hex.value
+                colorSchemeCode[index].textContent = data.colors[index].hex.value 
+            })
         })
-    })
-})
+}
 
-function ani(el) {
-    document.getElementById('img').className = 'classname';
-  }
+document.querySelectorAll('.getter').forEach(item => {
+    item.addEventListener('change', () => {      
+        colorFetcher()
+    })
+  })
+
+colorFetcher() /// to get colors on load
+
+///////////
+///END SECTION///
+    ////////////
+
+$(document).ready(function() {
+     /*  COPY TO CLIPBOARD  */
+    
+    // click the icon
+    $("[id^=copy-]").on("click", function(e) {
+      e.preventDefault;
+      
+      
+      // find the id number of the parent
+      let num = $(this).parent().attr("id").slice("color-".length);
+      let elem = $(".color-" + num);
+      
+      // copy the text of the background color
+      let $temp = $("<input>");
+      $("body").append($temp);
+      $temp.val(elem.children(":first").text()).select();
+      document.execCommand("copy");
+      $temp.remove();
+      
+      $(this).children(".tooltip").addClass("appear").delay(1000).queue(function( next ){
+        $(this).removeClass("appear"); 
+        $(this).dequeue();
+      });
+    });
+    
+});
+    
+    
+    
+    
+    
